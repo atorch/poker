ea627ff2ead44adde9b88a6c6bb59fa2295447ad
Adrian Torchiana <adrian.torchiana@gmail.com>
2020-09-07 16:08:21 -0700

    More hand ranks

--- poker/hands.py
+++ poker/hands.py
@@ -7,41 +7,63 @@ def sort_hand(hand):
     return sorted(hand, key=attrgetter("rank"))
 
 
+def is_straight(sorted_hand):
+
+    for i, card in enumerate(sorted_hand[:-1]):
+
+        next_card = sorted_hand[i + 1]
+
+        if card.rank + 1 != next_card.rank:
+            return False
+
+    return True
+
+
 def strength(hand):
 
     # TODO Finish this function
 
-    sorted_hand = sort_hand(hand)
-
     unique_suits = set(card.suit for card in hand)
+
+    sorted_hand = sort_hand(hand)
+    straight = is_straight(sorted_hand)
 
     # Note: the hand is a flush (either a straight flush or a regular flush)
     if len(unique_suits) == 1:
-        # TODO Distinguish flush and straight flush (and stronger and weaker flushes)
-        return 10
+
+        if straight:
+            # Straight flush
+            return 1000
+
+        # Regular flush
+        return 500
 
     rank_counter = Counter([card.rank for card in hand])
     most_common_ranks = rank_counter.most_common()
 
     if most_common_ranks[0][1] == 4:
         # Four of a kind
-        return 20
+        return 700
 
     if most_common_ranks[0][1] == 3 and most_common_ranks[1][1] == 2:
         # Full house
-        return 15
+        return 600
+
+    if straight:
+        # Regular straight (not a straight flush)
+        return 400
 
     if most_common_ranks[0][1] == 3:
         # Three of a kind
-        return 5
+        return 300
 
     if most_common_ranks[0][1] == 2 and most_common_ranks[1][1] == 2:
         # Two pair
-        return 4
+        return 200
 
     if most_common_ranks[0][1] == 2:
         # Pair
-        return 2
+        return 100
 
     # High card
-    return 0
+    return sorted_hand[-1].rank
--- tests/test_hands.py
+++ tests/test_hands.py
@@ -1,5 +1,5 @@
 from poker.cards import Suit, Rank, Card
-from poker.hands import strength, sort_hand
+from poker.hands import is_straight, strength, sort_hand
 
 
 def test_sort_hand():
@@ -20,7 +20,48 @@ def test_sort_hand():
     assert sorted_hand[3].rank == sorted_hand[4].rank == Rank.ACE
 
 
+def test_straights():
+
+    straight = [
+        Card(Rank.THREE, Suit.CLUBS),
+        Card(Rank.FOUR, Suit.CLUBS),
+        Card(Rank.FIVE, Suit.CLUBS),
+        Card(Rank.SIX, Suit.SPADES),
+        Card(Rank.SEVEN, Suit.DIAMONDS),
+    ]
+
+    non_straight = [
+        Card(Rank.THREE, Suit.CLUBS),
+        Card(Rank.FOUR, Suit.CLUBS),
+        Card(Rank.FIVE, Suit.CLUBS),
+        Card(Rank.SIX, Suit.SPADES),
+        Card(Rank.EIGHT, Suit.DIAMONDS),
+    ]
+
+    another_non_straight = [
+        Card(Rank.ACE, Suit.HEARTS),
+        Card(Rank.ACE, Suit.SPADES),
+        Card(Rank.THREE, Suit.CLUBS),
+        Card(Rank.SEVEN, Suit.DIAMONDS),
+        Card(Rank.TWO, Suit.CLUBS),
+    ]
+
+    assert is_straight(sort_hand(straight))
+    assert not is_straight(sort_hand(non_straight))
+    assert not is_straight(sort_hand(another_non_straight))
+
+    # TODO Also test low straights (ace, two, three, ...)
+
+
 def test_hand_strengh():
+
+    straight_flush = [
+        Card(Rank.ACE, Suit.HEARTS),
+        Card(Rank.JACK, Suit.HEARTS),
+        Card(Rank.QUEEN, Suit.HEARTS),
+        Card(Rank.KING, Suit.HEARTS),
+        Card(Rank.TEN, Suit.HEARTS),
+    ]
 
     flush = [
         Card(Rank.ACE, Suit.HEARTS),
@@ -28,6 +69,14 @@ def test_hand_strengh():
         Card(Rank.THREE, Suit.HEARTS),
         Card(Rank.SEVEN, Suit.HEARTS),
         Card(Rank.TWO, Suit.HEARTS),
+    ]
+
+    ace_high_straight = [
+        Card(Rank.ACE, Suit.HEARTS),
+        Card(Rank.JACK, Suit.SPADES),
+        Card(Rank.QUEEN, Suit.HEARTS),
+        Card(Rank.KING, Suit.HEARTS),
+        Card(Rank.TEN, Suit.HEARTS),
     ]
 
     three_of_a_kind = [
@@ -38,15 +87,23 @@ def test_hand_strengh():
         Card(Rank.JACK, Suit.HEARTS),
     ]
 
-    pair_of_jacks = [
+    two_pair = [
         Card(Rank.JACK, Suit.CLUBS),
+        Card(Rank.JACK, Suit.HEARTS),
+        Card(Rank.THREE, Suit.DIAMONDS),
+        Card(Rank.THREE, Suit.HEARTS),
+        Card(Rank.TWO, Suit.HEARTS),
+    ]
+
+    pair_of_jacks = [
         Card(Rank.THREE, Suit.DIAMONDS),
         Card(Rank.SEVEN, Suit.HEARTS),
         Card(Rank.TWO, Suit.HEARTS),
+        Card(Rank.JACK, Suit.CLUBS),
         Card(Rank.JACK, Suit.HEARTS),
     ]
 
-    high_card = [
+    queen_high = [
         Card(Rank.JACK, Suit.CLUBS),
         Card(Rank.EIGHT, Suit.HEARTS),
         Card(Rank.TWO, Suit.HEARTS),
@@ -54,9 +111,30 @@ def test_hand_strengh():
         Card(Rank.QUEEN, Suit.HEARTS),
     ]
 
+    jack_high = [
+        Card(Rank.JACK, Suit.CLUBS),
+        Card(Rank.EIGHT, Suit.HEARTS),
+        Card(Rank.TWO, Suit.HEARTS),
+        Card(Rank.THREE, Suit.DIAMONDS),
+        Card(Rank.FOUR, Suit.HEARTS),
+    ]
+
+    nine_high = [
+        Card(Rank.NINE, Suit.CLUBS),
+        Card(Rank.EIGHT, Suit.HEARTS),
+        Card(Rank.TWO, Suit.HEARTS),
+        Card(Rank.THREE, Suit.DIAMONDS),
+        Card(Rank.FOUR, Suit.HEARTS),
+    ]
+
     assert (
-        strength(flush)
+        strength(straight_flush)
+        > strength(flush)
+        > strength(ace_high_straight)
         > strength(three_of_a_kind)
+        > strength(two_pair)
         > strength(pair_of_jacks)
-        > strength(high_card)
+        > strength(queen_high)
+        > strength(jack_high)
+        > strength(nine_high)
     )
