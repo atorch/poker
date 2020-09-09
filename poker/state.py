881eef2fb0a058442fbf33c355b33749c9f8e8a1
Adrian Torchiana <adrian.torchiana@gmail.com>
2020-09-09 16:16:51 -0700

    Deal hole (private) cards

--- poker/state.py
+++ poker/state.py
@@ -4,12 +4,26 @@ from poker.cards import Suit, Rank, Card, FULL_DECK
 
 
 class State:
-
     def __init__(self, n_players=3):
 
         self.n_players = n_players
 
         self.shuffled_deck = sample(FULL_DECK, k=len(FULL_DECK))
+
+        # Note: these are the hidden (face down) cards which are hidden from other players
+        #  Player i observes only hole_cards[i]
+        self.hole_cards = self.deal_hole_cards()
+
+    def deal_two_cards(self):
+
+        first_card = self.shuffled_deck.pop()
+        second_card = self.shuffled_deck.pop()
+
+        return [first_card, second_card]
+
+    def deal_hole_cards(self):
+
+        return [self.deal_two_cards() for player in range(self.n_players)]
 
 
 def get_next_state(current_state, action):
--- tests/test_state.py
+++ tests/test_state.py
@@ -5,6 +5,9 @@ def test_get_next_state():
 
     initial_state = State()
 
+    n_cards_in_deck_after_initial_deal = 52 - initial_state.n_players * 2
+    assert len(initial_state.shuffled_deck) == n_cards_in_deck_after_initial_deal
+
     # TODO Actions should represent betting or folding
     action = 123
 
