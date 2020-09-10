ef96685e52c337b3e8ff57eb03ae9823bb4ffb94
Adrian Torchiana <adrian.torchiana@gmail.com>
2020-09-09 17:09:34 -0700

    Deal public cards

--- poker/state.py
+++ poker/state.py
@@ -13,15 +13,17 @@ class GameStage(IntEnum):
 
 
 class State:
-    def __init__(self, n_players=3):
+    def __init__(self, n_players=3, initial_wealth=100, big_blind=2, small_blind=1):
 
         self.n_players = n_players
 
         self.shuffled_deck = sample(FULL_DECK, k=len(FULL_DECK))
 
-        # Note: these are the hidden (face down) cards which are hidden from other players
+        # Note: these are the private (face down) cards which are hidden from other players
         #  Player i observes only hole_cards[i]
         self.hole_cards = self.deal_hole_cards()
+
+        self.public_cards = []
 
         self.game_stage = GameStage.PRE_FLOP
 
@@ -32,6 +34,8 @@ class State:
 
         self.has_folded = [False for player in range(self.n_players)]
 
+        self.wealth = [initial_wealth for player in range(self.n_players)]
+
         self.bets_by_stage = {
             stage: [[] for player in range(self.n_players)] for stage in GameStage
         }
@@ -39,44 +43,71 @@ class State:
     def get_next_player(self, current_player):
 
         next_player = (current_player + 1) % self.n_players
-        move_to_next_stage = False
+        all_others_have_folded = False
 
         while self.has_folded[next_player]:
 
             next_player = (next_player + 1) % self.n_players
 
             if next_player == current_player:
+                all_others_have_folded = True
                 break
 
-        return next_player, move_to_next_stage
+        return next_player, all_others_have_folded
 
-    def deal_two_cards(self):
+    def deal_k_cards(self, k=2):
 
-        first_card = self.shuffled_deck.pop()
-        second_card = self.shuffled_deck.pop()
-
-        return [first_card, second_card]
+        return [self.shuffled_deck.pop() for _ in range(k)]
 
     def deal_hole_cards(self):
 
-        return [self.deal_two_cards() for player in range(self.n_players)]
+        return [self.deal_k_cards(2) for player in range(self.n_players)]
 
-    def update(self, action):
+    def stage_is_complete(self, next_player):
 
-        player = self.current_player
-        stage = self.game_stage
+        if len(self.bets_by_stage[self.game_stage][self.current_player]) == 0:
+            return False
+
+        # Note: these are totals for the current stage only (e.g. player 1 has bet a total of $40 during the turn)
+        total_bet_current_player = sum(
+            self.bets_by_stage[self.game_stage][self.current_player]
+        )
+        total_bet_next_player = sum(self.bets_by_stage[self.game_stage][next_player])
+
+        return total_bet_current_player == total_bet_next_player
+
+    def update_has_folded_or_bets(self, action):
 
         if action < 0:
             # Note: negative bets indicate that the player is folding
-            self.has_folded[player] = True
+            self.has_folded[self.current_player] = True
 
         else:
-            self.bets_by_stage[stage][player].append(action)
+            self.bets_by_stage[self.game_stage][self.current_player].append(action)
 
-        next_player, move_to_next_stage = self.get_next_player(player)
+    def update(self, action):
 
-        if move_to_next_stage:
-            self.game_stage = self.game_stage + 1
+        self.update_has_folded_or_bets(action)
+
+        next_player, all_others_have_folded = self.get_next_player(self.current_player)
+
+        if self.stage_is_complete(next_player):
+
+            self.game_stage += 1
+
+            if self.game_stage == GameStage.FLOP:
+                self.public_cards.extend(self.deal_k_cards(3))
+            elif (
+                self.game_stage == GameStage.TURN or self.game_stage == GameStage.RIVER
+            ):
+                self.public_cards.extend(self.deal_k_cards(1))
 
         else:
-            self.current_player = next_player
+
+            if all_others_have_folded:
+                # TODO Finish this: if all other players have folded, the remaining player wins their bets
+                # TODO Remove wealth from other players
+                self.wealth[self.current_player] += 123
+
+            else:
+                self.current_player = next_player
