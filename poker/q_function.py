e7d2a124c09edaa2d8ec59e86068a4e464bfc141
Adrian Torchiana <adrian.torchiana@gmail.com>
2020-09-29 13:54:22 -0700

    Track number of deals

--- poker/q_function.py
+++ poker/q_function.py
@@ -8,14 +8,18 @@ from tensorflow.keras.layers import (
 
 def get_model(n_actions, n_inputs, n_units=64):
 
-    # This is a model for Q(private_state, action), i.e. an action-value function
+    # Note: this is a model for Q(private_state, action), i.e. an action-value function
 
+    # TODO Would it be cleaner to have a multi-input model, give it some structure,
+    #  have card array as one input, bets as another, etc?
+    #  Should also try one hot encoding discrete variables like the game stage
     input_layer = Input(shape=(n_inputs,))
 
     layer1 = Dense(n_units, activation="relu")(input_layer)
     layer2 = Dense(n_units, activation="relu")(layer1)
+    layer3 = Dense(n_units, activation="relu")(layer2)
 
-    output_layer = Dense(1, activation="linear")(layer2)
+    output_layer = Dense(1, activation="linear")(layer3)
 
     model = Model(inputs=input_layer, outputs=[output_layer])
 
